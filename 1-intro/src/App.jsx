aafde952fe96cca69685aaa9f169d8a00d4a1a44
lakshya <lakshyaparik99@gmail.com>
2025-07-13 18:23:29 +0530

    basic intro on components

---
+++ 1-intro/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { LikeButton, DislikeButton } from './Buttons.jsx'
+//default export
+function App() {
+  return (
+    <>
+    <h1>hello this is app text on jsx</h1>
+    <LikeButton />
+    <DislikeButton />
+    </>
+  )
+}
+
+export default App
---
+++ 1-intro/src/Buttons.jsx
@@ -0,0 +1,14 @@
+export function LikeButton() {
+  return (
+    <button className="btn">
+      Like
+    </button>
+  );
+}
+export function DislikeButton() {
+  return (
+    <button className="btn">
+      Dislike
+    </button>
+  );
+}
